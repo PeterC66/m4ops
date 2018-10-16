9cd1208ee34fe6faf8f5a7a5f9902beae643a3c8
PeterC66 <peter@pcooper.me.uk>
2018-10-16 10:46:20 +0100

    Remove Old React

---
+++ src/modules/framework/header/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <HeaderTitles opsdetails={{ OPSDetails }} />
+    <HeaderL1 opsdetails={{ OPSDetails }} />
+    <HeaderL2 />
+    <HeaderR1 />
+    <HeaderR2 />
+  </div>
+</template>
+
+<script>
+import HeaderTitles from './HeaderTitles';
+import HeaderR1 from './HeaderR1';
+import HeaderR2 from './HeaderR2';
+import HeaderL1 from './HeaderL1';
+import HeaderL2 from './HeaderL2';
+
+export default {
+  name: 'Header',
+  components: {
+    HeaderTitles,
+    HeaderR1,
+    HeaderR2,
+    HeaderL1,
+    HeaderL2,
+  },
+  props: {
+    OPSDetails: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
