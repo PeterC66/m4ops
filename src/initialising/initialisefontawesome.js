c2c4ba9b363e4891e7363377421aa5ac1f4ce681
PeterC66 <peter@pcooper.me.uk>
2018-10-16 06:07:26 +0100

    initialiseFontAwesome

---
+++ src/initialising/initialisefontawesome.js
@@ -0,0 +1,26 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Ensure icons are all imported here, and added to the library below
+import {
+  faArrowDown,
+  faArrowUp,
+  faArrowLeft,
+  faArrowRight,
+  faAngleDoubleUp,
+} from '@fortawesome/free-solid-svg-icons';
+// Note that there are problems using icons from free-regular-svg-icons and free-brands-svg-icons
+
+
+export default function initialiseFontAwesome(Vue) {
+// See import above
+  library.add(
+    faArrowDown,
+    faArrowUp,
+    faArrowLeft,
+    faArrowRight,
+    faAngleDoubleUp,
+  );
+
+  Vue.component('font-awesome-icon', FontAwesomeIcon); // Not Vue.use
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
 import Vue from 'vue';
 import VueLayers from 'vuelayers';
 import 'vuelayers/lib/style.css'; // needs css-loader
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-// Ensure icons are all imported here, and added to the library below
-import {
-  faArrowDown,
-  faArrowUp,
-  faArrowLeft,
-  faArrowRight,
-  faAngleDoubleUp,
-} from '@fortawesome/free-solid-svg-icons';
-// Note that there are problems using icons from free-regular-svg-icons and free-brands-svg-icons
 
 // Ensure Element components are all imported here, and 'used' below
 import {
@@ -24,6 +12,7 @@ import {
   Select,
 } from 'element-ui';
 
+import initialiseFontAwesome from './initialising/initialisefontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -39,16 +28,7 @@ Vue.use(Header);
 Vue.use(Main);
 Vue.use(Select);
 
-// See import above
-library.add(
-  faArrowDown,
-  faArrowUp,
-  faArrowLeft,
-  faArrowRight,
-  faAngleDoubleUp,
-);
-
-Vue.component('font-awesome-icon', FontAwesomeIcon); // Not Vue.use
+initialiseFontAwesome(Vue);
 
 Vue.config.productionTip = false;
 
