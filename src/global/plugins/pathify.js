ea957ad70e3081461fa17e276bfc9c02c78fac08
PeterC66 <peter@pcooper.me.uk>
2018-10-21 06:55:11 +0100

    remove pathify

--- src/global/plugins/pathify.js
+++
@@ -1,5 +0,0 @@
-import pathify from 'vuex-pathify';
-
-pathify.options.mapping = 'standard'; // Default
-
-export default pathify;
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import vuexApi from 'vuex-api';
 
-import pathify from '../global/plugins/pathify'; // from local version, originally from vuex-pathify
-
 import geography from './modules/geography';
 import mapping from './modules/mapping';
 import vuexApiGetters from './modules/vuexApi/vuexApiGetters';
@@ -13,7 +11,6 @@ Vue.use(Vuex);
 vuexApi.getters = vuexApiGetters;
 
 export default new Vuex.Store({
-  plugins: [pathify.plugin],
   modules: {
     vuexApi,
     geography,
