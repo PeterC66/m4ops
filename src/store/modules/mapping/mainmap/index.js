f9d6307488047159448cb6d201efb3c9af3c6344
PeterC66 <peter@pcooper.me.uk>
2018-10-22 05:59:11 +0100

    mainmap store done

--- src/store/modules/mapping/mainmap/index.js
+++ src/store/modules/mapping/mainmap/index.js
@@ -1,7 +1,8 @@
 import { isDefined } from '../../../../global/utils';
 
 import { LAYER_SET_REQUEST } from '../../../mutation-types';
-import { initialStateChosenLayers } from '../../../../initialising/initialState';
+import { initialStateChosenLayers }
+  from '../../../../initialising/initialState';
 
 const state = {
   chosenLdids: initialStateChosenLayers,
@@ -11,40 +12,10 @@ const mutations = {
   [LAYER_SET_REQUEST](moduleState, payload) {
     const { ldid, layerNumber } = payload;
     if (isDefined(layerNumber)) {
-      if (!isDefined(ldid)) { // indicating to delete the layer
-      }
+      moduleState.chosenLdids[layerNumber] = ldid || 'void';
     }
-    moduleState.chosenLdids = payload.mapDisplay;
-  }
-};
-
-/*
-
-const value = handleActions({
-  [setLayerRequest]: {
-    next: (state, action) => {
-      const { LayerDefs } = action;
-      const nextMainMap = _.cloneDeep(state.mainmap);
-      // for payload see above {ldid:string_index_into_LayerDefsArray, layerNumber: eg 0}
-      const layerindex = action.payload.layerNumber;
-      if (!isDefined(action.payload.ldid)) { // indicating to delete the layer
-        nextMainMap.getLayers().removeAt(layerindex);
-      } else {
-        if (isLayerDefined(nextMainMap, layerindex)) {
-          nextMainMap.getLayers().removeAt(layerindex);
-        }
-        const layerToInsert = layerFromDef(LayerDefs, action.payload.ldid);
-        nextMainMap.getLayers().insertAt(layerindex, layerToInsert);
-      }
-      return {
-        ...state,
-        mainmap: nextMainMap,
-      };
-    },
   },
-}, initialState);
-
-*/
+};
 
 const actions = {
 // payload is {ldid:string_index_into_LayerDefsArray, layerNumber: eg 0}
