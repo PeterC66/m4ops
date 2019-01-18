2b564d3d8db91d1db980cd76741a3c99eb3274fd
PeterC66 <peter@pcooper.me.uk>
2019-01-18 12:33:02 +0000

    Move to old

--- client/src/Old/users/index.js
+++ client/src/Old/users/index.js
@@ -1,19 +1,25 @@
 /*
 import Vue from 'vue';
-import Vuex from 'vuex';
+import VeeValidate from 'vee-validate';
 
-import { alert } from './alert.module';
-import { account } from './account.module';
-import { users } from './users.module';
+import { store } from './_store';
+// eslint-disable-next-line import/no-duplicates
+import { router } from './_helpers';
+import App from './app/App.vue';
 
-Vue.use(Vuex);
+// setup fake backend
+// eslint-disable-next-line import/no-duplicates
+import { configureFakeBackend } from './_helpers';
 
-// eslint-disable-next-line import/prefer-default-export
-export const store = new Vuex.Store({
-  modules: {
-    alert,
-    account,
-    users,
-  },
+Vue.use(VeeValidate);
+
+configureFakeBackend();
+
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
 });
 */
